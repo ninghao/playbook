ee1c2a91e1b816639eeed3bd7e4b940eea9a78e9
Phoenix <117663444@qq.com>
2015-03-01 23:08:32 +0800

    去掉了移动 hosts 文件

--- playbooks/files/shell/master.sh
+++ playbooks/files/shell/master.sh
@@ -1,4 +1,6 @@
 # 为 Master 安装 ansible
 sudo yum install ansible -y
-sudo mv /etc/ansible/hosts /etc/ansible/hosts_bak
+
+# 复制 inventory 到 Master 上
+# 这个文件里面是可以使用 ansible 控制的主机，默认这里包含 local 与 dev 这两台主机。
 sudo cp /vagrant/playbooks/inventory /etc/ansible/hosts -f
